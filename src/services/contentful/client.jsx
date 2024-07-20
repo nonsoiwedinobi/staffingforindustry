95f35bc71ff237ecc1d893cedfb5432c9c3fe7c0
Nonso Iwedinobi <nonsoiwedinobi@yahoo.com>
2024-07-20 18:01:23 +0100

    Debug

--- src/services/contentful/client.jsx
+++ src/services/contentful/client.jsx
@@ -1,8 +1,16 @@
+// services/contentful/client.js
+
 import { createClient } from "contentful";
 
+const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
+const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
+
+console.log("Contentful Space ID:", spaceId);
+console.log("Contentful Access Token:", accessToken);
+
 const client = createClient({
-  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+  space: spaceId,
+  accessToken: accessToken,
 });
 
 export default client;

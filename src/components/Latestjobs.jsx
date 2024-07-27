85f8ed1fcd8429e5919d6634532765403c2296d4
Nonso Iwedinobi <nonsoiwedinobi@yahoo.com>
2024-07-27 03:34:41 +0100

    changed content model

--- src/components/Latestjobs.jsx
+++ src/components/Latestjobs.jsx
@@ -110,7 +110,7 @@ const LatestJobs = () => {
                             <path d="M19 6a7.7 7.7 0 0 0-5.2-2A7.9 7.9 0 0 0 6 12c0 4.4 3.5 8 7.8 8 2 0 3.8-.8 5.2-2" />
                           </svg>
                         </span>
-                        <span>{job.fields.type}</span>
+                        <span>{job.fields.salary}</span>
                       </span>
                     </span>
                   </div>
